e75c92360e017832fa3508c7fb82e564cf92e854
jebulin <61864666+jebulin@users.noreply.github.com>
2020-05-01 13:12:03 +0530

    Updates

---
+++ precache-manifest.659460794eba4c9276aaca17d2711a7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5969dadf3a8a43cf4f7b88538c7b4a7e",
+    "url": "/project/index.html"
+  },
+  {
+    "revision": "836d9b2cf85a8c9e6ecf",
+    "url": "/project/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "605419d68ad0b0b351dd",
+    "url": "/project/static/css/main.5c300444.chunk.css"
+  },
+  {
+    "revision": "836d9b2cf85a8c9e6ecf",
+    "url": "/project/static/js/2.52887086.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/project/static/js/2.52887086.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "605419d68ad0b0b351dd",
+    "url": "/project/static/js/main.01906e50.chunk.js"
+  },
+  {
+    "revision": "e4e04f95801224b5bc62",
+    "url": "/project/static/js/runtime-main.89eb1b74.js"
+  }
+]);
